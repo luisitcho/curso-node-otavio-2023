3f840aea75dc13030a197b87a451a87a83c53825
Luis Henrique <62081612+luisitcho@users.noreply.github.com>
2023-09-13 20:43:36 -0300

    Add files via upload

---
+++ Webpack/src/index.js
@@ -0,0 +1,10 @@
+import somaoi, * as meuModulo from './module1';
+import './css/style.css';
+
+
+const p1 = new meuModulo.Pessoa('oi', 'beleza');
+console.log(somaoi(2, 2))
+console.log(p1);
+console.log(meuModulo.nome);
+console.log(meuModulo.sobrenome);
+console.log(meuModulo.idade);
