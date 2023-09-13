eeb8a4dd3bc8214ac6d163018d5f8ffa36b4b365
Luis Henrique <62081612+luisitcho@users.noreply.github.com>
2023-09-13 20:35:45 -0300

    Add files via upload

---
+++ aula04/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./controllers/homeController');
+const contatoController = require('./controllers/contatoController');
+// Home
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+// Contato
+route.get('/contato', contatoController.testeContato);
+
+module.exports = route;
